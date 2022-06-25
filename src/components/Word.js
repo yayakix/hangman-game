891b7447baf4be0ca6d9f21d9c0269e49c80878f
Iyana Marquez <yayakix@gmail.com>
2022-06-25 19:59:26 -0400

    word shows up on screen upon guess

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,4 +1,3 @@
-// generates a random word for the game
 import { useState, useEffect } from "react";
 import Game from "./Game";
 // how to show letters that were clicked on
@@ -7,62 +6,49 @@ import Game from "./Game";
 function Word(props) {
   const wordArr = props.word.split("");
   let listItems = wordArr.map((letter, index) => (
-    <li
-      className="letter"
-      key={index}      value={letter}
-    >
+    <li className="letter" key={index} value={letter}>
       {letter}
     </li>
   ));
-// create a blank array to populate
-const [blankArray, setBlankArray] =  useState([])
-let blankArr = []
-  for (let i = 0; i <= wordArr.length; i++) {
-    blankArr.push(" ");
-  }
+  // create a blank array to populate
+  const [blankArray, setBlankArray] = useState([]);
 
-  // console.log(wordArr);
-  const wordspot = [];
-  for (let i = 0; i < props.word.length; i++) {
-    wordspot.push("__");
-  }
-
-    // checks the index of guessed letter
-  const indices = [];
-  if (props.guess) {
-    let idx = wordArr.indexOf(props.guess);
-    while (idx != -1) {
-      indices.push(idx);
-      idx = wordArr.indexOf(props.guess, idx + 1);
+  useEffect(() => {
+    setBlankArray([]);
+    for (let i = 0; i < wordArr.length; i++) {
+      setBlankArray((prevState) => {
+        return [...prevState, " _ "];
+      });
     }
-    for(let i =0; i< indices.length; i++){
-        wordArr[indices[i]] = props.guess
-        // console.log(wordArr)
-        blankArr.splice(indices[i],0, props.guess)
-        
-      
-    }
-    
-    console.log("index of letters found:" + indices);
-  }
-     useEffect(() => {
-       setBlankArray(blankArr);
-     }, [props.guess]);
-console.log(blankArray)
+  }, [props.word]);
 
-//   var index = items.indexOf(3452);
-
-//   if (index !== -1) {
-//     items[index] = 1010;
-//   }
+  useEffect(
+    () => {
+      // console.log(props.guess);
+      const indexes = []
+      wordArr.forEach((element, index) => {
+        // get index of current guess
+        if( element === props.guess){
+          indexes.push(index)
+        }
+      });
+      let blankArrayCopy = [...blankArray]
+      indexes.forEach((index) => {
+        blankArrayCopy[index] = props.guess
+      })
+      setBlankArray(blankArrayCopy)
+      // console.log(indexes)
+    },
+    [props.guess]
+  );
 
   return (
     <div>
       <ul className="wordlist">{listItems}</ul>
-{blankArray}
-      {wordspot.map((x) => (
+      {blankArray}
+      {/* {wordspot.map((x) => (
         <div className="wordspot">{x}</div>
-      ))}
+      ))} */}
       <Game wordArr={wordArr} word={props.word} guess={props.guess} />
     </div>
   );
