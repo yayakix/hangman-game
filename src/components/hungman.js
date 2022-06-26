3da5102a97e73103b0d4b3331b17872812a7203a
Iyana Marquez <yayakix@gmail.com>
2022-06-25 21:29:55 -0400

    new hungman file

--- src/components/hungman.js
+++ src/components/hungman.js
@@ -2,16 +2,16 @@ import { useState, useEffect } from "react";
 
 // check to see if clicked button matches a letter in the chosen word
 function Hungman(props) {
-    const [man, setMan] = useState(null)
-    // const manPieces = ["O","|", "/","/"]
+  const [man, setMan] = useState(null);
+  // const manPieces = ["O","|", "/","/"]
 
-    useEffect(() => {
-        setMan(10 - props.lives);
-      }, [props.lives]);
+  useEffect(() => {
+    setMan(10 - props.lives);
+  }, [props.lives]);
   return (
     <div>
-    man has this many pieces:
-    {man}
+      man has this many pieces:
+      {man}
     </div>
   );
 }
